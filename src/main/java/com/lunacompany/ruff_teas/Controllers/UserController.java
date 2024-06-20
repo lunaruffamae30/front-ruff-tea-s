a0d1005f866ebf93fd1f5c588b49275d3d864739
lunaruffamae30 <ruffamaeluna323@gmail.com>
2024-06-20 15:34:11 +0800

    Configure to Database

--- src/main/java/com/lunacompany/ruff_teas/Controllers/UserController.java
+++ src/main/java/com/lunacompany/ruff_teas/Controllers/UserController.java
@@ -1,7 +1,11 @@
 package com.lunacompany.ruff_teas.Controllers;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lunacompany.ruff_teas.Model.UserModel;
@@ -14,5 +18,18 @@ public class UserController {
     @GetMapping("/user")
     public UserModel getuser(){
         return new UserModel(1, "ruffamae", "ruffamae@gmail.com", "ruffa2003");
+    }
+    //Fetch multiple data
+    @GetMapping("/users")
+    public List <UserModel>getusers(){
+        List<UserModel> users = new ArrayList<>();
+        users.add(new UserModel(1, "Ruffa Mae", "ruffamae@gmail.com","ruffa1"));
+        users.add(new UserModel(2, "Vhenus", "Vhenus@gmail.com","vhenus2"));
+        users.add(new UserModel(1, "Carla", "carla@gmail.com","carla3"));
+        return users;
+    }
+    @GetMapping("/user/{name}")
+    public UserModel getUserFromName(@PathVariable("name")String name){
+        return new UserModel(1, "ruffamae", "ruffamae@gmail.com", "ruffa20033"); 
     }
 }
