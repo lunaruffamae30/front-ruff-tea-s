e639fbde046beb2761815c4b45862a17ed4b0558
lunaruffamae30 <ruffamaeluna323@gmail.com>
2024-06-26 10:21:36 +0800

    finalized the activity 1

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int year =0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar:AppBar(
         title:Text('User Profile'),
-        backgroundColor: Colors.grey,
+        backgroundColor: Colors.white,
         centerTitle: true,
       ),
       body:Padding(
@@ -22,14 +28,14 @@ class Home extends StatelessWidget {
               children: <Widget>[
                 Center(
                   child: CircleAvatar(
-                    child: Image.asset('assets/woman.png'),
+                    backgroundImage: AssetImage('assets/woman.png'),
                     radius: 70.0,
                   ),
                 ),
                 Divider(
                   height: 30.0,
                   color: Colors.white,
-                  thickness: 15.0,
+                  thickness: 3.0,
                 ),
                 Row(
                   children: [
@@ -37,7 +43,7 @@ class Home extends StatelessWidget {
                       Icons.person_2,
                       color: Colors.white,
                     ),
-                    SizedBox(width:30.0,),
+                    SizedBox(width:3.0,),
                     Text(
                       'NAME',
                       style: TextStyle(
@@ -57,6 +63,13 @@ class Home extends StatelessWidget {
                   ),
                 ),
                 SizedBox(height: 30.0,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.calendar_month_sharp,
+                      color: Colors.white,
+                    ),
+                    SizedBox(width:3.0,),
                 Text(
                   'YEAR',
                   style: TextStyle(
@@ -65,14 +78,24 @@ class Home extends StatelessWidget {
                       letterSpacing:2.0
                   ),
                 ),
+                ],
+                ),
                 Text(
-                  'THIRD YEAR',
+                  '$year YEAR',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       color:Colors.white,
                       fontSize: 25.0
                   ),
                 ),
+
+                SizedBox(height: 30.0,),
+                Row(
+                  children: [
+                    Icon(
+                      Icons.email_sharp,
+                      color: Colors.white,
+                    ),
                 SizedBox(height: 30.0,),
                 Text(
                   'EMAIL',
@@ -81,6 +104,8 @@ class Home extends StatelessWidget {
                       color:Colors.white,
                       letterSpacing:2.0
                   ),
+                ),
+                ],
                 ),
                 Text(
                   'ruffamaeluna323@gmail.com',
@@ -97,8 +122,12 @@ class Home extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 ElevatedButton(
-                  onPressed: (){},
-                  child: Text('Add Year'),
+                  onPressed: (){
+                    setState(() {
+                      year += 1;
+                    });
+                  },
+                  child: Text( 'Add Year'),
                   style: ButtonStyle(
                     backgroundColor: WidgetStateProperty.all(Colors.white)
                   ),
@@ -111,3 +140,5 @@ class Home extends StatelessWidget {
     );
   }
 }
+
+
